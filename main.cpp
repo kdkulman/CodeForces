84c80d92ace0be0bbb4588f51f6e75abb032574a
Kevin Kulman <97306295+kdkulman@users.noreply.github.com>
2023-03-29 22:59:08 -0700

    daily problem

---
+++ main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main() {
+    int n, k;
+    cin >> n >> k;
+    int res = 0;
+    vector<int> list;
+    for (int i = 0; i < n; i++) {
+        int score;
+        cin >> score;
+        list.push_back(score);
+    }
+
+    int kthValue = 0;
+    for (int i = 0; i < n; i++) {
+        if (list[i] == 0) break;
+        if (i == k-1) kthValue = list[i];
+        if (i > k && list[i] != kthValue) break;
+        res++;
+    }
+
+    cout << res;
+}
+
+/*
+    test case
+    5 2
+    
+*/
