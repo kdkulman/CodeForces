8afbacd741bc32db5328a0a5c6e7a9b9d99840ce
Kevin Kulman <97306295+kdkulman@users.noreply.github.com>
2023-04-07 12:40:12 -0700

    Create SumOfCubes.cpp

---
+++ SumOfCubes.cpp
@@ -0,0 +1,22 @@
+//https://codeforces.com/problemset/problem/1490/C
+//Sum of Cubes
+
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    int t;
+    cin << t;
+
+    for (int i = 0; i < t; i++) {
+        long x;
+        cin << x;
+
+        
+    }
+}
